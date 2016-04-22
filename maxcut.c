59f37faceaef9310af223201b77a612fcba202de
DifferentSC <bert1234@snu.ac.kr>
2016-04-22 19:11:50 +0900

    Fix bugs and change parameters

--- maxcut.c
+++ maxcut.c
@@ -5,11 +5,11 @@
 
 #define TIME_OUT 180
 #define TIME_PADDING 20
-#define POPULATION_SIZE 100
-#define CHILDREN_SIZE 100
-#define MAX_VERTEX_SIZE 1000
+#define POPULATION_SIZE 800
+#define CHILDREN_SIZE 1000
+#define MAX_VERTEX_SIZE 800
 #define MAX_EDGE_SIZE 10000
-#define MUTATION_RATE 0.01
+#define MUTATION_RATE 0.005
 
 int vertex_size, edge_size;
 int edges[MAX_EDGE_SIZE][3];
@@ -26,7 +26,7 @@ void calculate_score() {
 
   for(i = 0; i < POPULATION_SIZE; i++) {
     score[i] = 0;
-    for(j = 0; j < vertex_size; j++) {
+    for(j = 0; j < edge_size; j++) {
       if (population_pool[i][edges[j][0]] != population_pool[i][edges[j][1]]) {
         score[i] += edges[j][2];
       }
@@ -34,9 +34,8 @@ void calculate_score() {
     if (score[i] < 0)
       score[i] = 0;
   }
-
   accum_score[0] = score[0];
-  for(i = 1; i < vertex_size; i++) {
+  for(i = 1; i < POPULATION_SIZE; i++) {
     accum_score[i] = accum_score[i-1] + score[i];
   }
 }
@@ -47,7 +46,7 @@ void calculate_children_score() {
 
   for(i = 0; i < CHILDREN_SIZE; i++) {
     children_score[i] = 0;
-    for(j = 0; j < vertex_size; j++) {
+    for(j = 0; j < edge_size; j++) {
       if (children_pool[i][edges[j][0]] != children_pool[i][edges[j][1]]) {
         children_score[i] += edges[j][2];
       }
@@ -62,7 +61,7 @@ void crossover(int p1, int p2, int child) {
   int crossover_point = rand() % vertex_size;
 
   memcpy(children_pool[child], population_pool[p1], crossover_point * sizeof(int));
-  memcpy(children_pool[child] + crossover_point, population_pool[p2] + crossover_point, (vertex_size - crossover_point) * vertex_size);
+  memcpy(children_pool[child] + crossover_point, population_pool[p2] + crossover_point, (vertex_size - crossover_point) * sizeof(int));
 }
 
 int find_max_chromosome() {
@@ -113,7 +112,7 @@ void mutate(int index) {
   for (i = mutation_start_point; i <= mutation_end_point; i++) {
     rev_seq[length - i + mutation_start_point - 1] = population_pool[index][i];
   }
-  memcpy(population_pool + mutation_start_point, rev_seq, length);
+  memcpy(population_pool[index] + mutation_start_point, rev_seq, length * sizeof(int));
   free(rev_seq);
 }
 
@@ -135,6 +134,9 @@ int main(int argc, char* argv[]) {
   fscanf(in, "%d %d", &vertex_size, &edge_size);
   for (i = 0; i < edge_size; i++) {
     fscanf(in, "%d %d %d", &edges[i][0], &edges[i][1], &edges[i][2]);
+    edges[i][0] -= 1;
+    edges[i][1] -= 1;
+    printf("%d %d %d\n", edges[i][0], edges[i][1], edges[i][2]);
   }
 
   // Create first population
@@ -146,7 +148,7 @@ int main(int argc, char* argv[]) {
 
   while(1) {
     // Selection & Crossover step
-    int score_sum = accum_score[vertex_size-1];
+    int score_sum = accum_score[POPULATION_SIZE-1];
     for(i = 0; i < CHILDREN_SIZE; i++) {
       // Selection
       int random_choice[2];
@@ -189,6 +191,7 @@ int main(int argc, char* argv[]) {
     // Mutation
     for (i = 0; i < POPULATION_SIZE * MUTATION_RATE; i++)
       mutate(rand() % POPULATION_SIZE);
+    calculate_score();
     time_t curr_time = clock();
     if (curr_time >= end_time)
       break;
@@ -203,11 +206,21 @@ int main(int argc, char* argv[]) {
   }
 
   FILE *out = fopen(argv[2], "w");
-  for (i = 0; i < POPULATION_SIZE -1; i++) {
-    fprintf(out, "%d ", population_pool[max_index][i]);
+  int *result = (int *)malloc(vertex_size * sizeof(int));
+  int result_length = 0;
+  for (i = 0; i < vertex_size; i++) {
+    if (population_pool[max_index][i] == 1) {
+      result[result_length] = i + 1;
+      result_length++;
+    }
   }
-  fprintf(out, "%d", population_pool[max_index][POPULATION_SIZE - 1]);
-
+  for (i = 0; i < result_length - 1; i++) {
+    fprintf(out, "%d ", result[i]);
+  }
+  fprintf(out, "%d", result[result_length-1]);
+  free(result);
+  // Should be deleted!
+  printf("Result: %d\n", max_value);
   fclose(in);
   fclose(out);
   return 0;
